486a27fab4fb05f974c4b85da2f675a23fd7edab
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-08-12 15:08:52 +0200

    Refactor ProductDetail.vue: updated HTML structure and added CSS classes

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -1,13 +1,19 @@
 <template>
-  <div v-if="loading">Loading...</div>
-  <div v-else>
-    <h1>{{ product.title }}</h1>
-    <img :src="product.image" alt="" />
-    <p>{{ product.description }}</p>
-    <p>{{ product.price }}</p>
-    <p>{{ product.category }}</p>
-    <p>{{ product.rating.rate }} ({{ product.rating.count }} reviews)</p>
-    <router-link to="/">Back to Products</router-link>
+  <div class="container mx-auto py-4">
+    <div v-if="loading" class="text-center">Loading...</div>
+    <div v-else class="max-w-4xl mx-auto">
+      <h1 class="text-3xl font-bold mb-4">{{ product.title }}</h1>
+      <img :src="product.image" alt="" class="w-full h-96 object-cover mb-4" />
+      <p class="text-gray-700 mb-2">{{ product.description }}</p>
+      <p class="text-xl font-semibold">{{ product.price }}</p>
+      <p class="text-gray-400 mb-2">{{ product.category }}</p>
+      <p class="text-gray-500">
+        {{ product.rating.rate }} ({{ product.rating.count }} reviews)
+      </p>
+      <router-link to="/" class="text-blue-500 hover:underline"
+        >Back to Products</router-link
+      >
+    </div>
   </div>
 </template>
 
