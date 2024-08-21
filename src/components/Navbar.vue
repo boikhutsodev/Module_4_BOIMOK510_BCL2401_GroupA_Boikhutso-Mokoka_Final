05855e517300a0c05329331926bfd16cb3ceefbb
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-08-21 13:50:04 +0200

    Refactored Navbar component: updated HTML structure, added toggle menu functionality, and modified CSS classes.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,15 +5,17 @@
     <div
       class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
     >
-      <router-link to="/" class="text-xl font-bold"
-        ><span
+      <router-link to="/" class="text-xl font-bold">
+        <span
           class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
-          >My Store</span
-        ></router-link
-      >
+        >
+          My Store
+        </span>
+      </router-link>
+
       <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-        <router-link :to="{ name: 'Comparison' }" class="hover:text-yellow-200"
-          ><button
+        <router-link :to="{ name: 'Comparison' }" class="hover:text-yellow-200">
+          <button
             type="button"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
@@ -21,11 +23,11 @@
           </button>
         </router-link>
         <button
-          data-collapse-toggle="navbar-sticky"
+          @click="toggleMenu"
           type="button"
           class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-sticky"
-          aria-expanded="false"
+          :aria-expanded="isMenuOpen.toString()"
         >
           <span class="sr-only">Open main menu</span>
           <svg
@@ -46,47 +48,70 @@
         </button>
       </div>
 
-      <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky"">
-        <router-link :to="{ name: 'Favorites' }" class="hover:text-yellow-200"
-          >Wishlist</router-link
+      <div
+        :class="{ hidden: !isMenuOpen, flex: isMenuOpen }"
+        class="items-center justify-between w-full md:flex md:w-auto md:order-1"
+        id="navbar-sticky"
+      >
+        <ul
+          class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
         >
-        <router-link :to="{ name: 'Cart' }" class="hover:text-yellow-200">
-          <svg
-            width="32px"
-            height="32px"
-            viewBox="0 0 32 32"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <!-- SVG content -->
-            <path
-              d="M7.5 18C8.32843 18 9 18.6716 9 19.5C9 20.3284 8.32843 21 7.5 21C6.67157 21 6 20.3284 6 19.5C6 18.6716 6.67157 18 7.5 18Z"
-              stroke="#1C274C"
-              stroke-width="1.5"
-            />
-            <path
-              d="M16.5 18.0001C17.3284 18.0001 18 18.6716 18 19.5001C18 20.3285 17.3284 21.0001 16.5 21.0001C15.6716 21.0001 15 20.3285 15 19.5001C15 18.6716 15.6716 18.0001 16.5 18.0001Z"
-              stroke="#1C274C"
-              stroke-width="1.5"
-            />
-            <path
-              d="M11 10.8L12.1429 12L15 9"
-              stroke="#1C274C"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M2 3L2.26121 3.09184C3.5628 3.54945 4.2136 3.77826 4.58584 4.32298C4.95808 4.86771 4.95808 5.59126 4.95808 7.03836V9.76C4.95808 12.7016 5.02132 13.6723 5.88772 14.5862C6.75412 15.5 8.14857 15.5 10.9375 15.5H12M16.2404 15.5C17.8014 15.5 18.5819 15.5 19.1336 15.0504C19.6853 14.6008 19.8429 13.8364 20.158 12.3075L20.6578 9.88275C21.0049 8.14369 21.1784 7.27417 20.7345 6.69708C20.2906 6.12 18.7738 6.12 17.0888 6.12H11.0235M4.95808 6.12H7"
-              stroke="#1C274C"
-              stroke-width="1.5"
-              stroke-linecap="round"
-            />
-          </svg>
-        </router-link>
-        <router-link :to="{ name: 'Login' }" class="hover:text-yellow-200"
-          >Login</router-link
-        >
+          <li>
+            <router-link
+              :to="{ name: 'Favorites' }"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            >
+              Wishlist
+            </router-link>
+          </li>
+          <li>
+            <router-link
+              :to="{ name: 'Cart' }"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            >
+              <svg
+                width="32px"
+                height="32px"
+                viewBox="0 0 32 32"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <!-- SVG content -->
+                <path
+                  d="M7.5 18C8.32843 18 9 18.6716 9 19.5C9 20.3284 8.32843 21 7.5 21C6.67157 21 6 20.3284 6 19.5C6 18.6716 6.67157 18 7.5 18Z"
+                  stroke="#1C274C"
+                  stroke-width="1.5"
+                />
+                <path
+                  d="M16.5 18.0001C17.3284 18.0001 18 18.6716 18 19.5001C18 20.3285 17.3284 21.0001 16.5 21.0001C15.6716 21.0001 15 20.3285 15 19.5001C15 18.6716 15.6716 18.0001 16.5 18.0001Z"
+                  stroke="#1C274C"
+                  stroke-width="1.5"
+                />
+                <path
+                  d="M11 10.8L12.1429 12L15 9"
+                  stroke="#1C274C"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M2 3L2.26121 3.09184C3.5628 3.54945 4.2136 3.77826 4.58584 4.32298C4.95808 4.86771 4.95808 5.59126 4.95808 7.03836V9.76C4.95808 12.7016 5.02132 13.6723 5.88772 14.5862C6.75412 15.5 8.14857 15.5 10.9375 15.5H12M16.2404 15.5C17.8014 15.5 18.5819 15.5 19.1336 15.0504C19.6853 14.6008 19.8429 13.8364 20.158 12.3075L20.6578 9.88275C21.0049 8.14369 21.1784 7.27417 20.7345 6.69708C20.2906 6.12 18.7738 6.12 17.0888 6.12H11.0235M4.95808 6.12H7"
+                  stroke="#1C274C"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                />
+              </svg>
+            </router-link>
+          </li>
+          <li>
+            <router-link
+              :to="{ name: 'Login' }"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            >
+              Login
+            </router-link>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
@@ -94,12 +119,19 @@
 
 <script>
 export default {
-  name: "Navbar",
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+  },
 };
 </script>
 
 <style scoped>
-nav {
-  z-index: 10;
-}
+/* Add any necessary styles here */
 </style>
