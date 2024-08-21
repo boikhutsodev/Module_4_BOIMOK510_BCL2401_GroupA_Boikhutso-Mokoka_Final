fd8f94ccbf5de174e3b6e675b6e66ad8997f0d4b
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-08-21 10:22:18 +0200

    Updated Home.vue: added image dimensions, rearranged rating component, and added SVG path for star icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@
       >
         <router-link :to="`/product/${product.id}`">
           <img
-            class="p-8 rounded-t-lg"
+            class="w-64 h-64 p-8 rounded-t-lg"
             :src="product.image"
             :alt="product.title"
           />
@@ -26,31 +26,33 @@
               {{ product.title }}
             </h5>
           </router-link>
-          <div class="flex items-center mt-2.5 mb-5">
-            <div class="flex items-center space-x-1 rtl:space-x-reverse">
-              <svg
-                v-for="i in 5"
-                :key="i"
-                class="w-4 h-4"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-                :class="{
-                  'text-yellow-300': i <= Math.round(product.rating.rate),
-                  'text-gray-200 dark:text-gray-600':
-                    i > Math.round(product.rating.rate),
-                }"
+          <div class="flex items-center space-x-1 rtl:space-x-reverse">
+            <div class="flex items-center mt-2.5 mb-5">
+              <div class="flex items-center space-x-1 rtl:space-x-reverse">
+                <svg
+                  v-for="i in 5"
+                  :key="i"
+                  class="w-4 h-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 20"
+                  :class="{
+                    'text-yellow-300': i <= Math.round(product.rating.rate),
+                    'text-gray-200 dark:text-gray-600':
+                      i > Math.round(product.rating.rate),
+                  }"
+                >
+                  <path
+                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
+                  />
+                </svg>
+              </div>
+              <span
+                class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3"
               >
-                <path
-                  d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-                />
-              </svg>
+                {{ product.rating.rate }}
+              </span>
             </div>
-            <span
-              class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3"
-            >
-              {{ product.rating.rate }}
-            </span>
           </div>
           <div class="flex items-center justify-between">
             <span class="text-3xl font-bold text-gray-900 dark:text-white">
